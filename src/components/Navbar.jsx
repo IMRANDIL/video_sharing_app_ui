d02702d137cea28e07a12541ca9986a92dc666fb
IMRANDIL <aliimranadil2@gmail.com>
2022-07-02 08:22:13 +0530

    yh

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,48 @@
 import React from "react";
 import styled from "styled-components";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import SearchIcon from "@mui/icons-material/Search";
 
-const Container = styled.div``;
-const Wrapper = styled.div``;
-const Search = styled.div``;
-const Input = styled.input``;
+const Container = styled.div`
+  position: sticky;
+  top: 0;
+  background-color: ${({ theme }) => theme.bgLight};
+  height: 56px;
+`;
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  position: relative;
+  padding: 0px 20px;
+  height: 100%;
+`;
+const Search = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 50%;
+  position: absolute;
+  left: 0px;
+  right: 0px;
+  margin: auto;
+  padding: 5px;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+`;
+const Input = styled.input`
+  border: none;
+  background-color: transparent;
+  width: 100%;
+`;
 const Button = styled.button`
   padding: 5px 15px;
   background-color: transparent;
   border: 1px solid #3ea6ff;
   color: #3ea6ff;
-  width: 100%;
   border-radius: 3px;
   font-weight: 500;
   cursor: pointer;
-  margin-top: 9px;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -22,7 +50,19 @@ const Button = styled.button`
 `;
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Container>
+      <Wrapper>
+        <Search>
+          <Input placeholder="Search" />
+          <SearchIcon />
+        </Search>
+        <Button>
+          <AccountCircleIcon /> SIGN IN
+        </Button>
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default Navbar;
